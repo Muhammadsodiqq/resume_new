0f796ac8d0ac31980d215a3833ae278ca6c6f0a1
Muhammadsodiqq <75038821+Muhammadsodiqq@users.noreply.github.com>
2021-06-18 20:06:12 +0500

    add some backend

--- js/fetch.js
+++ js/fetch.js
@@ -1,7 +1,10 @@
+
 ;(async () => {
     let h3Name = document.querySelector("#h3Name")
     let dev = document.querySelector("#dev")
     let h1Name = document.querySelector("#h1Name")
+    let bio1 = document.querySelector("#bio1")
+    let bio2 = document.querySelector("#bio2")
     let response = await fetch("https://resume-muhammad.herokuapp.com/about-center",{
         headers:{
             "Content-type":"aplication/json"
@@ -14,16 +17,31 @@
     h3Name.childNodes[2]. textContent = response.name.split(" ")[1]
     dev.textContent = response.dev
     h1Name.textContent = response.name
+    bio1.textContent = response.biografy1
+    bio2.textContent = response.biografy2
+    // console.log(bio1,bio2.textContent);
 })()
 
 ;(async () => {
-    let response = await fetch("https://resume-muhammad.herokuapp.com/us",{
+    let telegram = document.querySelector("#telegram")
+    let phone = document.querySelector("#phone")
+    let emaill = document.querySelector("#emaill")
+    let namee = document.querySelector("#namee")
+    let bio2 = document.querySelector("#bio2")
+    let birth = document.querySelector("#birth")
+    let city = document.querySelector("#city")
+    let response = await fetch("https://resume-muhammad.herokuapp.com/infos",{
         headers:{
             "Content-type":"aplication/json"
         }
     })
     response = await response.json()
-    // for(let i of response) {
-        // console.log();
-    // }
+    let info = response[0]
+    telegram.textContent = info.Telegram
+    phone.textContent = info.Phone
+    emaill.textContent = info.Email
+    namee.textContent = info.Name
+    birth.textContent = info.Birth
+    city.textContent =  info.City
+
 })()
